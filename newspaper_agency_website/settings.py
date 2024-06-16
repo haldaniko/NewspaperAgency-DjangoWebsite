8255b5caf89491b7474c1f393a82b216f1836fe8
haldaniko <haldaniko@gmail.com>
2024-06-16 16:50:38 +0200

    modify settings.py

--- newspaper_agency_website/settings.py
+++ newspaper_agency_website/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,7 +83,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
